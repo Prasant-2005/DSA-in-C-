80e6f512297874fc1371c356bf293a752182ffa9
Prasant <prashantjee1075@gmail.com>
2024-11-20 17:51:28 +0530

    done

---
+++ condition&loops/switchStatement/primeNumber.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+// 0 -> Not a Prime no.
+// 1 -> Prime no.
+bool isPrime(int n)
+{
+    for (int i = 2; i < n; i++)
+    {
+        if (n % i == 0)
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    if (isPrime(n))
+    {
+        cout << "is a prime number" << endl;
+    }
+    else
+    {
+        cout << "not a prime number" << endl;
+    }
+    return 0;
+}
