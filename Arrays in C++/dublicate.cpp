7b68d493e6671becdeb6e34a0f2d02271f8ff2d6
Prasant <prashantjee1075@gmail.com>
2024-11-25 00:07:13 +0530

    done

---
+++ Arrays in C++/dublicate.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int findDuplicate(vector<int> &arr) {
+    int ans = 0;
+    for (int i = 0; i < arr.size(); i++) {
+        ans = ans ^ arr[i];
+    }
+    for (int i = 1; i < arr.size(); i++) {
+        ans = ans ^ i;
+    }
+    return ans;
+}
+
+int main() {
+    vector<int> arr = {1, 3, 4, 2, 3};
+    cout << "Duplicate number is: " << findDuplicate(arr) << endl;
+    return 0;
+}
