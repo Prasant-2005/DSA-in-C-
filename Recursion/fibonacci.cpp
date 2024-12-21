146f564e83a937dfc43f2adbfbbcdd67cd6db7a9
Prasant <prashantjee1075@gmail.com>
2024-12-21 08:14:51 +0530

    Done

--- Recursion/fibonacci.cpp
+++ Recursion/fibonacci.cpp
@@ -2,6 +2,14 @@
 using namespace std;
 int fibo(int n){
     // base case
+    if(n == 0){
+        return 0;
+    }
+    if(n == 1){
+        return 1;
+    }
+    int ans =  fibo(n-1) + fibo(n-2); 
+    return ans;
 }
 int main() {
     int n;
